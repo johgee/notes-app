a078d9d20fc91b86a6e13efed2b7771ee4036c23
Grace Joh <graceeunaejoh@gmail.com>
2022-04-14 13:34:58 -0700

    notes-app frontend

---
+++ src/views/NoteSelectors.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="note-selectors">
+    <NoteSelector v-for="note in notes" v-bind:key="note.id" v-bind:note="note" />
+  </div>
+</template>
+
+<script>
+import NoteSelector from "./NoteSelector.vue";
+
+export default {
+  components: {
+    NoteSelector,
+  },
+  data: function () {
+    return {
+      notes: [
+        { id: 1, body: "first test", imestamps: Date.now() },
+        { id: 2, body: "second test", imestamps: Date.now() },
+        { id: 3, body: "third test", imestamps: Date.now() },
+        { id: 4, body: "fourth test", imestamps: Date.now() },
+      ],
+    };
+  },
+};
+</script>
